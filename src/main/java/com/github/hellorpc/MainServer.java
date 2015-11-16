5e38aadd6ed1402d443a31f35e94a5b03a28a2e9
George H <georgeinfo@126.com>
2015-11-16 17:12:32 +0800

    First commit.

---
+++ src/main/java/com/github/hellorpc/MainServer.java
@@ -0,0 +1,9 @@
+package com.github.hellorpc;
+
+public class MainServer {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/com/github/hellorpc/protocol/Header.java
@@ -0,0 +1,13 @@
+/*
+ * Author : George <GeorgeNiceWorld@gmail.com> | <Georgeinfo@163.com>
+ * Copyright (C) George (http://www.georgeinfo.com), All Rights Reserved.
+ */
+package com.github.hellorpc.protocol;
+
+/**
+ *
+ * @author George <Georgeinfo@163.com>
+ */
+public interface Header {
+    
+}
